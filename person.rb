0112ea5b4ac232b52de772d5b781a94de104bbc4
blessedjasonmwanza <mwanzabj@gmail.com>
2022-03-17 22:41:29 +0200

    inherit Nameable in person class

--- person.rb
+++ person.rb
@@ -1,18 +1,24 @@
-require_relative './nameable'
-class Person
-  attr_accessor :name, :age
-  attr_reader :id
+require './nameable'
+require './decorator'
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+class Person < Nameable
+  def initialize(age, name = 'Unknown', parrent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
-    @parent_permission = parent_permission
-    @nameable = Nameable.new
+    @parrent_permission = parrent_permission
   end
 
-  def validate_name
-    @name = @nameable.correct_name(name)
+  attr_accessor :name, :age
+  attr_reader :id
+
+  def can_use_services?
+    is_of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   private
@@ -20,15 +26,4 @@ class Person
   def of_age?
     @age >= 18
   end
-
-  public
-
-  def can_use_services?
-    of_age? || parent_permission == true
-  end
 end
-
-person = Person.new(100, 'blessedjasonmanza')
-person.validate_name
-
-puts person.name
