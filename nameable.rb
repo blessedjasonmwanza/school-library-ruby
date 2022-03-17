338e2ca012df18b049f2765f354138329804c4a8
blessedjasonmwanza <mwanzabj@gmail.com>
2022-03-17 22:20:03 +0200

    Fix indetation linter errors

--- nameable.rb
+++ nameable.rb
@@ -1,6 +1,5 @@
 class Nameable
-    def correct_name(name)
-      name.length >= 10 ? name[0..9].capitalize : name.capitalize
-    end
+  def correct_name(name)
+    name.length >= 10 ? name[0..9].capitalize : name.capitalize
   end
-  
+end
